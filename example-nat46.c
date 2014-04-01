1726a102d24f6d7ab56233b01b08571d64984ab0
Andrew Yourtchenko <ayourtch@gmail.com>
2014-04-01 19:40:12 +0200

    Add rudimentary CLI input

--- example-nat46.c
+++ example-nat46.c
@@ -30,10 +30,47 @@ int pcap_read_ev(int idx, dbuf_t *d, void *p) {
   return d->dsize;
 }
 
+int cli_read_ev(int idx, dbuf_t *di, void *p) {
+  int i;
+  char *op;
+  dbuf_t *d = NULL;
+  char *cmd = di->buf;
+  debug(0,0, "Cmd: '%s'", cmd);
+  if (NULL == cmd) {
+    return 1;
+  }
+  op = strtok(cmd, " ");
+  if (NULL == op) {
+    return 1;
+  }
+
+  if (0 == strcmp(op, "h")) {
+    debug(0,0, "d <level>");
+  }
+  if (0 == strcmp(op, "d")) {
+  }
+  if (0 == strcmp(op, "q") || 0 == strcmp(op, "\x03")) {
+    detach_stdin();
+    printf("\n\n\n");
+    exit(1);
+  }
+
+  if (0 == strcmp(op, "r")) {
+    uint32_t xid = atol(strtok(NULL, " "));
+    uint16_t offs = atoi(strtok(NULL, " "));
+    int mf = atoi(strtok(NULL, " "));
+    char *data = strtok(NULL, " ");
+    char *now_s = strtok(NULL, " ");
+  }
+  return (d ? d->dsize : 0) || 1;
+}
+
 
 int main(int argc, char *argv[]) {
   int timeout = 0;
   sock_handlers_t *hdl;
+  int cli;
+
   if (argc < 2) { 
     fprintf(stderr, "Usage: %s <dev>\n", argv[0]);
     exit(1);
@@ -46,6 +83,12 @@ int main(int argc, char *argv[]) {
 
   tuni = attach_tun_interface(NULL);
   set_v4_idx(tuni);
+
+  cli = attach_cli(attach_stdin(1));
+  hdl = cdata_get_handlers(cli);
+  hdl->ev_read = cli_read_ev;
+  debug(0,0, "Press Ctrl-C to quit");
+
 
   set_debug_level(DBG_GLOBAL, 0); 
 
